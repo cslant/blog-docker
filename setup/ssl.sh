7d23ae27d47c2953b5c7e875b58b4cd581c73f49
Tan Nguyen <tannp27@gmail.com>
2024-07-13 18:33:12 +0700

    fix: the condition to generate domain certs

--- setup/ssl.sh
+++ setup/ssl.sh
@@ -39,48 +39,43 @@ ssl() {
       mkdir -p nginx/server/certs
     fi
 
-    create_certs
+    generate_cert_files
 
-    cd "$SOURCE_DIR" || exit
-    cd nginx/conf/customs || exit
+    CUSTOMS_PATH="$CURRENT_DIR/nginx/conf/customs"
+    for domain in "${DOMAINS[@]}"; do
+      SSL_CONTENT="ssl_certificate /var/www/certs/${domain}.local.pem;\nssl_certificate_key /var/www/certs/${domain}-key.pem;"
 
-    DOMAIN=(
-      "${BLOG_DOMAIN}"
-      "${BLOG_API_DOMAIN}"
-      "${BLOG_ADMIN_DOMAIN}"
-    )
-     for domain in "${DOMAIN[@]}"; do
-       SSL_CONTENT="ssl_certificate /var/www/certs/${domain}.local.pem;\nssl_certificate_key /var/www/certs/${domain}-key.pem;"
-       if [ ! -s "${domain}.ssl.conf" ]; then
-         echo -e "$SSL_CONTENT" | tee "${domain}.ssl.conf"
-         echo "Create successful file ${domain}.ssl.conf"
-       else
-         echo -e "$SSL_CONTENT" | tee "${domain}.ssl.conf"
-         echo "${domain}.ssl.conf already exists and update."
-       fi
-     done
+      echo -e "$SSL_CONTENT" | tee "$CUSTOMS_PATH/${domain}.ssl.conf"
+      echo "${domain}.ssl.conf setup successfully."
+    done
   fi
 }
 
-function create_certs() {
-  BLOG_CERTS_PATH="$CURRENT_DIR/nginx/server/certs/${BLOG_DOMAIN}.pem"
+function generate_cert_files() {
+  CERTS_PATH="$CURRENT_DIR/nginx/server/certs"
+  cd "$CERTS_PATH" || exit
+
+  BLOG_CERTS_PATH="$CERTS_PATH/${BLOG_DOMAIN}.pem"
   if [ ! -f "$BLOG_CERTS_PATH" ]; then
       mkcert "${BLOG_DOMAIN}"
   else
     echo "Certificate for ${BLOG_DOMAIN} already exists."
   fi
 
-  API_CERTS_PATH="$CURRENT_DIR/nginx/server/certs/${BLOG_API_DOMAIN}.pem"
+  API_CERTS_PATH="$CERTS_PATH/${BLOG_API_DOMAIN}.pem"
   if [ ! -f "$API_CERTS_PATH" ]; then
     mkcert "${BLOG_API_DOMAIN}"
   else
     echo "Certificate for ${BLOG_API_DOMAIN} already exists."
   fi
 
-  ADMIN_CERTS_PATH="$CURRENT_DIR/nginx/server/certs/${BLOG_ADMIN_DOMAIN}.pem"
+  ADMIN_CERTS_PATH="$CERTS_PATH/${BLOG_ADMIN_DOMAIN}.pem"
   if [ ! -f "$ADMIN_CERTS_PATH" ]; then
      mkcert "${BLOG_ADMIN_DOMAIN}"
   else
      echo "Certificate for ${BLOG_ADMIN_DOMAIN} already exists."
   fi
+
+  echo "Certificates generated successfully."
+  cd "$CURRENT_DIR" || exit
 }
