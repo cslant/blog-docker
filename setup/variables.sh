894d9697c2453c4afb7a512ae8e17b0cf9dbc287
Tan Nguyen <tannp27@gmail.com>
2024-08-02 13:08:07 +0700

    fix: clean code

--- setup/variables.sh
+++ setup/variables.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 GIT_SSH_URL=${GIT_SSH_URL:-git@github.com:cslant}
+
 # shellcheck disable=SC2034
 GITHUB_TOKEN=${GIT_TOKEN:-ghp_1234567890}
+
 # shellcheck disable=SC2034
 CURRENT_DIR=$(pwd)
+
 # shellcheck disable=SC2034
 SOURCE_DIR=$(readlink -f "$SOURCE_CODE_PATH")
+
 # shellcheck disable=SC2034
 BLOG_PACKAGE_REPO_NAMES=(
   'blog-api-package'
